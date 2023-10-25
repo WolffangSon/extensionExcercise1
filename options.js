d1b7f223b9cad87dd0e0c99243287cd328e0378f
Wolffang Nicolas Son Hernandez <wolffang.son@telusinternational.com>
2023-10-25 12:11:25 -0600

    fin

--- options.js
+++ options.js
@@ -1,6 +1,21 @@
+const hcInput = document.getElementById("hc-input");
+const shrinkInput = document.getElementById("shrinkage");
+const btnCalculate = document.getElementById("btnCalculate");
+const resultText = document.getElementById("result");
 
-function test1(){
-    alert("a ver");
-}
-const boton = document.getElementById("calculate");
-boton.onclick(test1())
+btnCalculate.addEventListener("click", () => {
+  const hcValue = parseFloat(hcInput.value);
+  const shrinkValue = parseFloat(shrinkInput.value);
+
+  if (!isNaN(hcValue) && !isNaN(shrinkValue)) {
+    const result = hcValue - (hcValue * shrinkValue / 100);
+   alert("Son: " + result + " FTEs")
+
+  } else {
+   alert("Ingresa valores numéricos válidos")
+  }
+});
+
+
+
+
